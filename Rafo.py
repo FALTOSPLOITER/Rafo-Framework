78ffb3fc1b30bd532bc719d7f36ed8e8b6331235
NITHIEN AACHINTHYA <180380357+FALTOSPLOITER@users.noreply.github.com>
2024-11-28 21:58:33 +0530

    Update Rafo.py

--- Rafo.py
+++ Rafo.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 import sys
-import readline 
+import readline
 from run import *
 from welcome import welcome
 from colorama import Fore, Back, Style
@@ -11,26 +11,26 @@ comm = ''
 
 # Loop function to read commands and act accordingly
 def handleCommands():
-        global hist_list  
-        global comm 
-        while True:
-                try:
-                        comm = input(f'\nRafo > ')
-                        if comm != 'history':
-                                hist_list.append(comm.strip())
-                        elif comm.strip() == 'clear':
-                                subprocess.call("clear", shell=True)
-                        if comm.strip() == 'history':
-                                if len(hist_list) > 0:
-                                        print('Last commands:')
-                                        for i in range(0, len(hist_list)):
-                                                print(hist_list[i]) 
-                                else:
-                                        print('No commands history.')
-                        else:
-                                os.system(f'python3 run.py {comm}') 
-                except KeyboardInterrupt:
-                        sys.exit('\n^C\n')
+    global hist_list
+    global comm
+    while True:
+        try:
+            comm = input(f'\nRafo > ')
+            if comm != 'history':
+                hist_list.append(comm.strip())
+            elif comm.strip() == 'clear':
+                subprocess.call("clear", shell=True)
+            if comm.strip() == 'history':
+                if len(hist_list) > 0:
+                    print('Last commands:')
+                    for i in range(0, len(hist_list)):
+                        print(hist_list[i])
+                else:
+                    print('No commands history.')
+            else:
+                os.system(f'python3 run.py {comm}')
+        except KeyboardInterrupt:
+            sys.exit('\n^C\n')
 
 def main():
     # if no arguments are present, run the welcome() function
