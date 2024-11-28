b924763241d8b8d9d4f813aa9e40a7cf361739b9
NITHIEN AACHINTHYA <180380357+FALTOSPLOITER@users.noreply.github.com>
2024-11-28 21:47:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,58 +1,32 @@
-# Show available options
+#!/bin/bash
 
-echo "1. Debian-based (Debian, Ubuntu, Kali, ParrotOS, Pop!_OS, Linux Mint, Deepin, Elementary OS, Zorin OS, MX Linux, etc) "
+# Show available options
+echo "1. Debian-based (Debian, Ubuntu, Kali, ParrotOS, Pop OS, Linux Mint, Deepin, Elementary OS, Zorin OS, MX Linux, etc)"
 echo "2. RHEL-based (Red Hat Enterprise Linux, Fedora, CentOS, Rocky Linux, AlmaLinux, Oracle Linux, ClearOS, etc)"
 echo "3. Arch-based (Arch, Black Arch, Manjaro, etc)"
 
 read -p "Enter your distro [1/2/3]: " choice
 
-if test "$choice" = "1"
-then
-
+if [ "$choice" = "1" ]; then
     # Install necessary packages for Debian-based distros
-
-    sudo apt-get install nmap
-    sudo apt-get install python3-pip
-    
-     
-elif test "$choice" = "2"
-then
-
+    sudo apt-get update
+    sudo apt-get install -y nmap python3-pip
+elif [ "$choice" = "2" ]; then
     # Install necessary packages for RHEL-based distros
-
-    sudo dnf install nmap
-    sudo dnf install python3-pip
-
-elif test "$choice" = "3"
-then
-
+    sudo dnf install -y nmap python3-pip
+elif [ "$choice" = "3" ]; then
     # Install necessary packages for Arch-based distros
-
-    sudo pacman -S nmap
-    sudo pacman -S python3-pip
-
+    sudo pacman -Sy --noconfirm nmap python3-pip
 else
-     echo "Invalid choice. Operation cancelled."
-     exit 
+    echo "Invalid choice. Operation cancelled."
+    exit
 fi
 
 # Install all necessary pip packages
+sudo pip3 install gnureadline colorama ipaddress python-nmap ipinfo scapy shodan python-whois paramiko netfilterqueue
 
-sudo pip3 install gnureadline
-sudo pip3 install colorama
+# Add main .py file to the system path
+sudo cp Rafo.py /usr/bin/Rafo
+sudo chmod +x /usr/bin/Rafo
 
-sudo pip3 install ipaddress
-sudo pip3 install python-nmap
-sudo pip3 install ipinfo
-sudo pip3 install scapy
-sudo pip3 install shodan
-sudo pip3 install python-whois
-sudo pip3 install paramiko
-sudo pip3 install netfilterqueue
-
-
-# Add main .py file to the system path (later)
-#cp *.py /usr/bin
-#cp methods/*.py /usr/bin
-#mv /usr/bin/hawk.py /usr/bin/hawk
-#chmod 755 /usr/bin/hawk
+echo "Rafo has been installed successfully!"
