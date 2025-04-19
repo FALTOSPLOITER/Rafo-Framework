097917a2507794fddfa93eb6d08a5a8bd1b5ad67
NITHIEN AACHINTHYA <180380357+FALTOSPLOITER@users.noreply.github.com>
2025-04-19 10:29:44 +0530

    Rafo.py

--- Rafo.py
+++ Rafo.py
@@ -9,6 +9,12 @@ from colorama import Fore, Back, Style
 hist_list = []
 comm = ''
 
+def check_root():
+    if os.geteuid() != 0:
+        print(f'\n{Fore.RED}Error: This script requires root privileges to run properly.{Style.RESET_ALL}')
+        print(f'Please run with sudo: {Fore.YELLOW}sudo python3 Rafo.py{Style.RESET_ALL}')
+        sys.exit(1)
+
 # Loop function to read commands and act accordingly
 def handleCommands():
     global hist_list
@@ -27,18 +33,22 @@ def handleCommands():
                     print('No commands history.')
             else:
                 hist_list.append(comm.strip())
-                os.system(f'python3 run.py {comm}')
+                # Use subprocess.run instead of os.system for better security
+                subprocess.run(['python3', 'run.py'] + comm.strip().split(), check=True)
         except KeyboardInterrupt:
             sys.exit('\n^C\n')
+        except subprocess.CalledProcessError:
+            print(f'{Fore.RED}Error executing command. Please check your input.{Style.RESET_ALL}')
 
 def main():
+    # Check for root privileges first
+    check_root()
+    
     # if no arguments are present, run the welcome() function
     if len(sys.argv) == 1:
         welcome()
 
-    if not 'SUDO_UID' in os.environ.keys():
-        print(f'\nDisclaimer: {Fore.YELLOW}It is recommended to run this script with root privileges.{Style.RESET_ALL}')
-
     handleCommands()
 
-main()
+if __name__ == '__main__':
+    main()
